035ab6cac4010d2924436ecc589505844323a8e6
philbot5000 <philbot5000@gmail.com>
2014-06-26 15:45:28 -0600

    cleaned up css

--- src/views/ContentView.js
+++ src/views/ContentView.js
@@ -5,6 +5,7 @@ define(function(require, exports, module) {
         StateModifier = require('famous/modifiers/StateModifier'),
         HeaderFooter  = require('famous/views/HeaderFooterLayout'),
         ImageSurface  = require('famous/surfaces/ImageSurface'),
+        Flipper    = require("famous/views/Flipper"),
         FastClick     = require('famous/inputs/FastClick');
 
     function ContentView() {
@@ -15,6 +16,7 @@ define(function(require, exports, module) {
         _createBody.call(this);
         _createBacking.call(this);
         _setListeners.call(this);
+        //_setFlipperListener.call(this);
     }
 
     ContentView.prototype = Object.create(View.prototype);
@@ -41,7 +43,7 @@ define(function(require, exports, module) {
     function _createHeader() {
       var backgroundSurface = new Surface({
         properties: {
-          backgroundColor: '#09347A',
+          backgroundColor: '#ccc',
           borderBottom: '2px solid #F1632A'
         }
       });
@@ -65,9 +67,13 @@ define(function(require, exports, module) {
         align: [0, 0.5]
       });
 
-      var logoSurface = new ImageSurface({
+      var logoSurface = new Surface({
         size: [44,33],
-        content: 'img/logo.png'
+        content: '<i class="fa fa-rebel"></i>',
+        properties: {
+          fontSize: '30px',
+          color: 'red'
+        }
       });
 
       var logoModifier = new StateModifier({
@@ -83,29 +89,58 @@ define(function(require, exports, module) {
 
     function _createBody() {
 
-        this.bodySurface = new Surface({
-            size : [undefined, undefined],
-            content: '<i class="fa fa-rebel"></i>',
-            properties: {
-              backgroundColor: '#fff',
-              fontSize: '45px',
-              textAlign: 'center',
-              color: 'red'
-            }
-        });
+      this.bodySurface = new Surface({
+          size : [undefined, undefined],
+          content: 'Drag Here.',
+          properties: {
+            backgroundColor: 'white',
+            fontSize: '45px',
+            textAlign: 'center',
+            color: 'red'
+          }
+      });
 
-        var bodySurfaceModifier = new StateModifier({
+        // this.flipper = new Flipper();
+        //
+        // this.rebelSurface = new Surface({
+        //     size : [200, 200],
+        //     content: '<i class="fa fa-rebel"></i>',
+        //     properties: {
+        //       backgroundColor: 'red',
+        //       fontSize: '45px',
+        //       textAlign: 'center',
+        //       color: '#fff'
+        //     }
+        // });
+        //
+        // var empireSurface = new Surface({
+        //     size : [200, 200],
+        //     content: '<i class="fa fa-empire"></i>',
+        //     properties: {
+        //       backgroundColor: 'blue',
+        //       color: '#fff',
+        //       fontSize: '45px',
+        //       textAlign: 'center'
+        //     }
+        // });
+
+        // this.flipper.setFront(this.rebelSurface);
+        // this.flipper.setBack(empireSurface);
+
+        //this.bodySurface.add(flipper);
+
+        this.bodySurfaceModifier = new StateModifier({
           origin: [0.5, 0.5],
           align: [0.5, 0.5]
         });
 
-        this.layout.content.add(bodySurfaceModifier).add(this.bodySurface);
+        this.layout.content.add(this.bodySurfaceModifier).add(this.bodySurface);
     }
 
     function _createBacking() {
         var backing = new Surface({
             properties: {
-                backgroundColor: 'black',
+                backgroundColor: 'blue',
                 boxShadow: '0 0 20px rgba(0,0,0,0.5)'
             }
         });
@@ -119,6 +154,15 @@ define(function(require, exports, module) {
       }.bind(this));
 
       this.bodySurface.pipe(this._eventOutput);
+    }
+
+    function _setFlipperListener() {
+      var toggle = false;
+      this.rebelSurface.on('click', function(){
+          var angle = toggle ? 0 : Math.PI;
+          this.flipper.setAngle(angle, {curve : 'easeOutBounce', duration : 500});
+          toggle = !toggle;
+      }.bind(this));
     }
 
     module.exports = ContentView;
